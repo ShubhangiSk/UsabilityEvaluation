717201afa3eee7725360ed1d1d170a02ce8c6cdc
Shubhangi <shubhangi.khasnis7@gmail.com>
2018-11-16 20:04:30 +0530

    Added blue part recognition

--- img_processing.py
+++ img_processing.py
@@ -6,7 +6,7 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\t
 # Path of working folder on Disk
 src_path = "C:/Users/Shubhangi/Desktop/FINAL_YEAR_PROJECT/UsabilityEvaluation/"
 
-def get_string(img_path):
+def get_all_text(img_path):
     # Read image with opencv
     img = cv2.imread(img_path)
 
@@ -36,8 +36,43 @@ def get_string(img_path):
 
     return result
 
+def get_blue_part(img_path):
+    # Read image with opencv
+    img = cv2.imread(img_path)
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # define range of blue color in HSV
+    lower_blue = np.array([100,25,30])
+    upper_blue = np.array([130,255,255])
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-print('--- Start recognize text from image ---')
-print(get_string(src_path + "sampleblue.png"))
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(img,img, mask= mask)
+    # Convert to gray
+    if img.all() != None:
+       img = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    res = cv2.dilate(img, kernel, iterations=1)
+    res = cv2.erode(img, kernel, iterations=1)
+    # Write the image after apply opencv to do some ...
+    ret,thresh1 = cv2.threshold(res,127,255,cv2.THRESH_BINARY_INV)
+    cv2.imwrite(src_path + "thres.png", thresh1)
+    
+
+    # Recognize text with tesseract for python
+    result = pytesseract.image_to_string(Image.open(src_path + "thres.png"))
+
+    # Remove template file
+    #os.remove(temp)
+
+    return result
+
+print('--- Start recognize all text from image ---')
+print(get_all_text(src_path + "sampleblue.png"))
+print("------ Done -------")
+print('\n\n--- Start recognize blue text from image ---')
+print(get_blue_part(src_path + "sampleblue.png"))
 
 print("------ Done -------")
